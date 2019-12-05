f6b87d921846450edc339cff5c44a275217ac334
susanpallmann <38414619+susanpallmann@users.noreply.github.com>
2019-12-05 00:54:05 -0500

    Update ui.js

--- ui.js
+++ ui.js
@@ -1,2 +1,7 @@
-function dayNightToggle() {
-}
+$('#day-night-toggle').click(function dayNightToggle() {
+  var page = $(html);
+  if (page.hasClass('night') {
+    page.removeClass('night');
+  } else {
+    page.addClass('night');
+});
