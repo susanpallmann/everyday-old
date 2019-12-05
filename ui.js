b1739bd2227124dda538232e99558a7ba7cb96d2
susanpallmann <38414619+susanpallmann@users.noreply.github.com>
2019-12-05 01:29:43 -0500

    Update ui.js

--- ui.js
+++ ui.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
   $('#day-night-toggle').click(function () {
     console.log("clicked me!");
-    var page = $(document);
+    var page = $(document.body);
     var dayNightToggle = $('#day-night-toggle');
     if (dayNightToggle.attr('aria-checked') === 'false') {
       page.addClass('night');
