85a5c71c63405da98d14f578535f6dc1fa0d0263
susanpallmann <38414619+susanpallmann@users.noreply.github.com>
2019-12-05 01:04:01 -0500

    Update ui.js

--- ui.js
+++ ui.js
@@ -1,11 +1,18 @@
 $('#day-night-toggle').click(function dayNightToggle() {
-  var page = $(html);
-  if (page.hasClass('night') {
-    page.removeClass('night');
-    $('#day-mode').addClass('invisible');
-    $('#night-mode').removeClass('invisible');
-  } else {
-    page.addClass('night');
-    $('#day-mode').removeClass('invisible');
-    $('#night-mode').addClass('invisible');
+  console.log("clicked me!");
+//  var page = $(html);
+//  if (page.hasClass('night') {
+//    page.removeClass('night');
+//    $('#day-mode').addClass('invisible');
+//    $('#night-mode').removeClass('invisible');
+//  } else {
+//    page.addClass('night');
+//    $('#day-mode').removeClass('invisible');
+//    $('#night-mode').addClass('invisible');
 });
+
+
+//.getAttribute('aria-checked') === 'true') {
+//    document.documentElement.classList.remove('dark');
+//    themeToggle.setAttribute('aria-checked', 'false');
+//    localStorage.setItem('theme', 'light');
