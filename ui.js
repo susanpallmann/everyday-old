a55f730a23e8dae972620678fa32cc83bb3fc963
susanpallmann <38414619+susanpallmann@users.noreply.github.com>
2019-12-05 01:14:17 -0500

    Update ui.js

--- ui.js
+++ ui.js
@@ -1,21 +1,18 @@
 $(document).ready(function(){
   $('#day-night-toggle').click(function () {
     console.log("clicked me!");
+    var page = $(html);
+    var dayNightToggle = $('#day-night-toggle');
+    if (dayNightToggle.getAttribute('aria-checked') === 'false') {
+      page.addClass('night');
+      dayNightToggle.setAttribute('aria-checked') === 'true');
+      $('#day-mode').addClass('invisible');
+      $('#night-mode').removeClass('invisible');
+    } else {
+      page.removeClass('night');
+      dayNightToggle.setAttribute('aria-checked') === 'false');
+      $('#day-mode').removeClass('invisible');
+      $('#night-mode').addClass('invisible');
+    }
   });
 });
-//  var page = $(html);
-//  if (page.hasClass('night') {
-//    page.removeClass('night');
-//    $('#day-mode').addClass('invisible');
-//    $('#night-mode').removeClass('invisible');
-//  } else {
-//    page.addClass('night');
-//    $('#day-mode').removeClass('invisible');
-//    $('#night-mode').addClass('invisible');
-
-
-
-//.getAttribute('aria-checked') === 'true') {
-//    document.documentElement.classList.remove('dark');
-//    themeToggle.setAttribute('aria-checked', 'false');
-//    localStorage.setItem('theme', 'light');
