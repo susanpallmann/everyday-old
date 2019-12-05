28733a373bd9bfa4b8f0a5cfa667b54bd15a89e8
susanpallmann <38414619+susanpallmann@users.noreply.github.com>
2019-12-05 01:16:18 -0500

    Update ui.js

--- ui.js
+++ ui.js
@@ -5,12 +5,12 @@ $(document).ready(function(){
     var dayNightToggle = $('#day-night-toggle');
     if (dayNightToggle.getAttribute('aria-checked') === 'false') {
       page.addClass('night');
-      dayNightToggle.setAttribute('aria-checked') === 'true');
+      dayNightToggle.setAttribute('aria-checked') === 'true';
       $('#day-mode').addClass('invisible');
       $('#night-mode').removeClass('invisible');
     } else {
       page.removeClass('night');
-      dayNightToggle.setAttribute('aria-checked') === 'false');
+      dayNightToggle.setAttribute('aria-checked') === 'false';
       $('#day-mode').removeClass('invisible');
       $('#night-mode').addClass('invisible');
     }
